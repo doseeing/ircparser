dceae09eb3ed03269d190fd7437a4e6f93cf67e1
doseeing <doseeing@gmail.com>
2024-12-09 15:56:59 +0800

    fix: avoid tags include symbol error

--- src/lib.rs
+++ src/lib.rs
@@ -172,7 +172,7 @@ pub fn parse(text: &str) -> ParseResult<VecDeque<Line>> {
         }
 
         // Parse source component.
-        if line.find(':') == Some(idx) {
+        if line[idx..].find(':') == Some(0) {
             let end_idx = find_index(line, ' ', idx).unwrap();
             source = Some(line[idx..end_idx].to_string());
             idx = end_idx + 1;
